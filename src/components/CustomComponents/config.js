ab33d43fd7ed1b1df38b12757e37637b5d564e9b
eethaldevteam <eethalnaditsolutions@gmail.com>
2026-02-27 16:56:28 +0530

    payroll fifnish

--- src/components/CustomComponents/config.js
+++ src/components/CustomComponents/config.js
@@ -1,4 +1,5 @@
 export const config = {
-  Api: "http://127.0.0.1:8001", //Local server
-  // Api: 'https://neo-physio-node.onrender.com'
+  // Api: "http://127.0.0.1:8001", //Local server
+  // Api: 'https://neo-physio-node.onrender.com', // live server
+  Api: 'https://neo-physio-node-con7.onrender.com', // backup server
 };
