f46c610d1f3942d4fec46f5dc3feb1527d5adcd6
eethaldevteam <eethalnaditsolutions@gmail.com>
2026-03-04 19:17:00 +0530

    hostinger server

--- src/components/CustomComponents/config.js
+++ src/components/CustomComponents/config.js
@@ -1,5 +1,6 @@
 export const config = {
   // Api: "http://127.0.0.1:8001", //Local server
-  Api: "https://neo-physio-node.onrender.com", // live server
+  // Api: "https://neo-physio-node.onrender.com", // live server
   // Api: "https://neo-physio-node-con7.onrender.com", // backup server
+  Api: "https://neodesk-node.eniscloud.in", // Hostinger server
 };
