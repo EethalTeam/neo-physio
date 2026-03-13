9442eebbbc3efc9ee6df68f4f8cdcb4685896895
eethaldevteam123 <eethalnaditsolutions@gmail.com>
2026-03-13 15:44:50 +0530

    repots and reviews and session config version

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -385,7 +385,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
 
               <span className="flex flex-col text-blue-600">
                 <span className="text-xl font-bold">NEO DESK</span>
-                <span className="text-xs text-blue-300">V-1.4.4</span>
+                <span className="text-xs text-blue-300">V-1.4.6</span>
               </span>
             </motion.div>
           ) : (
