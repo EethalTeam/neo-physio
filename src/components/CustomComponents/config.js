4913ae7395aadb8205cc29497ad40213e714a20a
eethaldevteam123 <eethalnaditsolutions@gmail.com>
2026-04-07 19:19:15 +0530

    payroll business mobile responsive pyroll

--- src/components/CustomComponents/config.js
+++ src/components/CustomComponents/config.js
@@ -1,9 +1,9 @@
-export const config = {
-  Api: "http://127.0.0.1:8002", //Local server
-  //   // Api: "https://neo-physio-node.onrender.com", // live server
-  //   // Api: "https://neo-physio-node-con7.onrender.com", // backup server
-  //   // Api: "https://neodesk-node.eniscloud.in", // Hostinger server
-};
 // export const config = {
-//   Api: "",
+// Api: "http://127.0.0.1:8002", //Local server
+//   // Api: "https://neo-physio-node.onrender.com", // live server
+//   // Api: "https://neo-physio-node-con7.onrender.com", // backup server
+//   // Api: "https://neodesk-node.eniscloud.in", // Hostinger server
 // };
+export const config = {
+  Api: "",
+};
